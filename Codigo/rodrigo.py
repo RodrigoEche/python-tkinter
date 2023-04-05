36f345120a373e3e6842c917897503a2bc1b2eee
RodrigoEche <echerodrigo@yahoo.com>
2023-04-05 17:42:36 -0300

    Push: 2023-April-05-17:42:36

--- Codigo/rodrigo.py
+++ Codigo/rodrigo.py
@@ -4,3 +4,11 @@ print(sys.path)
 
 import platform as plataforma
 print("Sistema operativo ---> "+plataforma.system())
+
+#pag 398
+#https://pypi.org/project/tabulate/
+from tabulate import tabulate
+print(tabulate([["Nombre","Edad"],["Rodrigo",51],["Yanina",47]],    headers="firstrow"))
+print("   ")
+print(tabulate( [["Mascota","Nombre","Raza"],["Perro","Pancha","Galgo"],
+                ["Gato","Violeta","Negrus"],["Gato","Michin","Salvagius"]],    headers="firstrow" ))
